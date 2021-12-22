8c8ad4da82475e6e699b0a1ee2d2fa6429261fa2
viraj <virazssolanki@gmail.com>
2021-12-22 18:11:33 +0530

    auth with wavekeeper

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,25 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {}
+        this.authFunc = this.authFunc.bind(this);
+    }
+    authFunc(){
+        const authData = { data: "blockchain bitch"};
+        if (WavesKeeper) {
+            WavesKeeper.auth(authData)
+            .then(auth => {
+                console.log(auth);
+            }).catch(error => {
+                console.error( error );
+            })
+        } else {
+            alert("where waves keeper man! come on");
+        }
     }
     render() {
         return (
             <div className='container'>
-                <input className='btn' type="submit" value="Alert" onClick={() => {alert("boom");}}/>
+                <input className='btn' type="submit" value="Auth" onClick={this.authFunc}/>
             </div>
         )
     }
